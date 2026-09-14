df7d94c85b1ba05f08c148b9c95fd6e3e1b85dd3
Deva Sai Nandini Mandapalli <devasainandinimandapalli@gmail.com>
2026-09-14 20:37:04 +0530

    Enable Neon PostgreSQL SSL

--- backend/src/db/pool.js
+++ backend/src/db/pool.js
@@ -6,7 +6,8 @@ const pool = new Pool({
   database: process.env.DATABASE_NAME,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  max: 10,
+    ssl: { rejectUnauthorized: false },
+    max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 5000,
 });
